4aa458caf556598d924528fc8a7922f5b9095367
Raphaël Bournhonesque <raphael0202@yahoo.fr>
2015-07-24 12:20:05 +0200

    First release

--- leboncoin/core.py
+++ leboncoin/core.py
@@ -4,8 +4,11 @@
 import re
 import time
 import sqlite3
+import json
+from outbox import Outbox, Email
 import requests
 from bs4 import BeautifulSoup
+
 
 def scan(url, max_nb_connection=10):
     nb_connection = 0
@@ -21,7 +24,7 @@ def scan(url, max_nb_connection=10):
             connected = True
 
     if nb_connection > max_nb_connection:
-        raise ConnectionError("{} failed connections, aborting.".format(max_nb_connection))
+        raise ConnectionError("{} failed connections, aborting.".format(max_nb_connection)) from e
 
     soup = BeautifulSoup(r.text, "lxml")
     ad_list = soup.find("div", class_="list-lbc").find_all("a")
@@ -37,6 +40,8 @@ def scan(url, max_nb_connection=10):
         for d in date_tag:
             date.append(d.string.strip("\n\t "))
 
+        date = ", ".join(date)
+
         price = ad.find("div", class_="price")
         if price is not None:
             price = price.string.strip("\n\t ")
@@ -44,40 +49,94 @@ def scan(url, max_nb_connection=10):
         category = ad.find("div", class_="category")
         if category is not None:
             category = category.string.strip("\n\t ")
+            category = category.replace("\n", "")
+            category = category.replace("\t", "")
+            category = category.replace("\r", "")
 
         placement = ad.find("div", class_="placement")
         if placement is not None:
             placement = placement.string.strip("\n\t ")
+            placement = placement.replace("\n", "")
+            placement = placement.replace("\t", "")
+            placement = placement.replace("\r", "")
 
 
         ads.append({"title": title,
                     "category": category,
                     "price": price,
                     "placement": placement,
-                    "date": date
+                    "date": date,
+                    "link": link
                     })
 
-    for ad in ads:
-        print(ad)
+    return ads
 
-def send_email(ad):
-    pass
+def send_email(ad, config_file):
 
-def get_links_db():
-    pass
+    with open(config_file, 'r') as f:
+        config = json.load(f)
 
-def create_db(db_name="ads.db"):
+    port = config["port"]
+    server = config["server"]
+    fromaddr = config["fromaddr"]
+    toaddrs = config["toaddrs"]
+    username = config["username"]
+    password = config["password"]
+
+    subject = "Nouvelle annonce sur Leboncoin : {}".format(ad["title"])
+
+    body = """Bonjour,\nUne nouvelle annonce sur Leboncoin satisfaisant vos critères de recherche vient d'être publiée.\n\n
+    Titre : {title}\nDate de publication : {date}\nPrix : {price}\nCatégorie : {category}\nLocalisation : {placement}\n""".format(**ad)
+
+    outbox = Outbox(username=username, password=password, server=server, port=port)
+    email = Email(subject=subject, body=body, recipients=toaddrs)
+    outbox.send(email)
+
+
+def add_ad(ad, db_name):
     conn = sqlite3.connect(db_name)
     c = conn.cursor()
-    c.execute()
+    links = c.execute("""INSERT INTO ADS VALUES (:title, :link, :category, :price, :placement, :date)""", ad)
+    conn.commit()
+    conn.close()
 
-def watch(url, time_interval=5):
+def get_links_db(db_name):
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+
+    links = list(c.execute("""SELECT LINK FROM ADS"""))
+    links = [tup[0] for tup in links]
+    conn.close()
+    return links
+
+def create_db(db_name):
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+    c.execute("""CREATE TABLE ADS
+               (TITLE TEXT, LINK TEXT, CATEGORY TEXT, PRICE TEXT, PLACEMENT TEXT, DATE_PUBLICATION TEXT)""")
+    conn.commit()
+    conn.close()
+
+def watch(url, db_name, config_file, init=False):
+    print("Scanning web page...")
     ad_list = scan(url)
-    links = get_links_db()
+    print("Getting URL from database...")
+    links = get_links_db(db_name)
 
     for ad in ad_list:
-        if ad["href"] not in links:
-            send_email(ad)
-            add_ad(ad)
+        if ad["link"] not in links:
+            if not init:
+                send_email(ad, config_file)
 
-    time.sleep(time_interval * 60)
+            print("Adding ad '{}' to database".format(ad["title"]))
+            add_ad(ad, db_name)
+
+
+def start(url, db_name, config_file="config.json", time_interval=5):
+    create_db(db_name)
+    watch(url, db_name, config_file, init=True)
+
+    while True:
+        print("Sleeping for {} min".format(time_interval))
+        time.sleep(time_interval * 60)
+        watch(url, db_name, config_file)
